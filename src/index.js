838ffb2d3e84440f722bc5e472e1f74a0c1867bb
seungyun cho <cho31250@gmail.com>
2016-09-28 02:30:13 +0900

    Add example code in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import Caculater from './calculator';
+
+const c = new Caculater();
+
 const newDiv = document.createElement('h1');
-const newContent = document.createTextNode("Success");
-newDiv.appendChild(newContent);
+const br = document.createElement('br');
+
+const add = document.createTextNode(`4 + 2 = ${c.add(4, 2)}`);
+const sub = document.createTextNode(`4 - 2 = ${c.sub(4, 2)}`);
+
+newDiv.appendChild(add);
+newDiv.appendChild(br);
+newDiv.appendChild(sub);
 
 document.body.appendChild(newDiv);
